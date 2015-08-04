e077b78f24e04d8970f355855c5b88c9cec8006a
Timothy Sabat <timothy.sabat@gmail.com>
2015-08-04 14:56:21 -0700

    some scripting for after_install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-chown -R deploy:deploy /var/www/tinge/versions
+set -ex
 
-cd /var/www/tinge/versions/
+# get to version dir
+cd /var/www/tinge/versions
+
+# get last directory and chop the trailing slash
 VERSION=$(ls -d */ | head -1 | sed -e 's#/$##')
+
+# symlink the dir
 ln -snf /var/www/tinge/versions/$VERSION /var/www/tinge/current
 
+# set owner
+chown -R deploy:deploy /var/www/tinge/versions
+
+# restart
 passenger-config restart-app /var/www/tinge/current
