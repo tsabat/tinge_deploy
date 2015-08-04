941776aa8d699f2f4e6249130868a3a20970bc09
Timothy Sabat <timothy.sabat@gmail.com>
2015-08-04 16:49:51 -0700

    fix the appsepc

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # get last directory and chop the trailing slash
-VERSION=$(cat /var/www/tinge/current_deployment_version.txt)
+VERSION=$(cat /var/www/tinge/version.txt)
 
 mv /var/www/tinge/current_deployment /var/www/tinge/versions/$VERSION
 # symlink the dir
